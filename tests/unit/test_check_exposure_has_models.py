f41f2cf5a4c33a282d83d8c292ce52b03fc8ac74
larryandria <larryandriamampianina@FRART0452M.local>
2022-06-23 14:56:58 +0200

    feat: add unit tests

---
+++ tests/unit/test_check_exposure_has_models.py
@@ -0,0 +1,77 @@
+
+import pytest
+
+from pre_commit_dbt.check_exposure_has_models import main
+
+TESTS = (  # type: ignore
+    (
+        """
+version: 2
+exposures:
+  - name: weekly_jaffle_metrics
+    type: dashboard
+    maturity: high
+    url: https://bi.tool/dashboards/1
+    description: >
+      Did someone say "exponential growth"?
+
+    depends_on:
+      - ref('fct_orders')
+      - ref('dim_customers')
+      - source('gsheets', 'goals')
+
+    owner:
+      name: Claire from Data
+      email: data@jaffleshop.com
+    """,
+        0,
+        [],
+    ),
+    (
+        """
+version: 2
+exposures:
+  - name: weekly_jaffle_metrics
+    type: dashboard
+    maturity: high
+    url: https://bi.tool/dashboards/1
+    description: >
+      Did someone say "exponential growth"?
+
+    depends_on:
+
+    owner:
+      name: Claire from Data
+      email: data@jaffleshop.com
+    """,
+        1,
+        [],
+    ),
+    (
+        """
+version: 2
+exposures:
+  - name: weekly_jaffle_metrics
+    type: dashboard
+    maturity: high
+    url: https://bi.tool/dashboards/1
+    description: >
+      Did someone say "exponential growth"?
+
+    owner:
+      name: Claire from Data
+      email: data@jaffleshop.com
+    """,
+        1,
+        [],
+    ),
+)
+
+@pytest.mark.parametrize(("schema_yml", "expected_status_code", "ignore"), TESTS)
+def test_check_column_desc_is_same(schema_yml, expected_status_code, ignore, tmpdir):
+    yml_file = tmpdir.join("schema.yml")
+    yml_file.write(schema_yml)
+    input_args = [str(yml_file)]
+    input_args.extend(ignore)
+    status_code = main(input_args)
+    assert status_code == expected_status_code
