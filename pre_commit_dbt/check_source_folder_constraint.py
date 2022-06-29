ee6cd13c44489d3086d770e4e3be8e71de0514dd
CharbelNaba <charbelnaba@gmail.com>
2022-06-29 10:49:08 +0200

    modified the hook's functionnality to match specification

--- pre_commit_dbt/check_source_folder_constraint.py
+++ pre_commit_dbt/check_source_folder_constraint.py
@@ -7,24 +7,24 @@ from pre_commit_dbt.utils import add_filenames_args
 from pre_commit_dbt.utils import get_source_path
 
 """
- Checks if sources are defined in the folder specified by the user
+ Checks if sources are not defined in the folder specified by the user
  Arguments:
      paths: a sequence of paths
-     source_folder_path: string that represents the specified folder's path
+     source_folder_path: string that represents the specified unwanted folder's path
  Returns:
-     A status code (0 if the sources are placed in the correct folder and 1 otherwise)
+     A status code (0 if the sources are not placed in the specified folder and 1 otherwise)
  """
 def check_source_path(paths: Sequence[str], source_folder_path: str) -> int:
     status_code = 0
     ymls = [Path(path) for path in paths]
 
     source_paths = get_source_path(ymls)
-    misplaced_sources = {source_path for source_path in source_paths if source_folder_path not in str(source_path)}
+    misplaced_sources = {source_path for source_path in source_paths if source_folder_path in str(source_path)}
     for misplaced_source in misplaced_sources:
          status_code = 1
          print(
              f"{misplaced_source}: "
-             f"is not in the directory passed as argument for the hook",
+             f"is in the directory passed as argument for the hook",
          )
     return status_code
 
@@ -34,7 +34,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument(
         "--source-folder",
         required=True,
-        help="Directory in which sources should be defined.",
+        help="Directory in which sources should not be defined.",
     )
     args = parser.parse_args(argv)
     return check_source_path(args.filenames, source_folder_path=args.source_folder)
--- tests/unit/test_check_source_folder_constraint.py
+++ tests/unit/test_check_source_folder_constraint.py
@@ -14,7 +14,7 @@ sources:
       - name: orders
       - name: customers
     """,
-        1,
+        0,
         "/models/sources.yml",
         "sources",
     ),
@@ -28,14 +28,24 @@ sources:
       - name: orders
       - name: customers
     """,
-        0,
+        1,
         "/models/sources.yml",
         "models",
     ),
 )
 
-@pytest.mark.parametrize(("schema_yml", "expected_status_code", "source_path", "desired_path"), TESTS)
-def test_check_source_folder_constraint(schema_yml, expected_status_code, source_path, desired_path, tmpdir):
+''''
+Tests the check-source-folder-constraint hook
+ Arguments:
+     schema_yml: source content
+     expected_status_code: status code that should be returned by the test for the current test case
+     source_path: path of the yaml file where the source is defined
+     undesired_path: path/name of the folder where we don't want the source to be defined
+ Returns:
+     A status code (0 if the sources are not placed in the specified folder and 1 otherwise)
+'''
+@pytest.mark.parametrize(("schema_yml", "expected_status_code", "source_path", "undesired_path"), TESTS)
+def test_check_source_folder_constraint(schema_yml, expected_status_code, source_path, undesired_path, tmpdir):
     source_dir=tmpdir.mkdir(source_path[:source_path.rfind(os.sep)]) # source_path[:source_path.rfind(os.sep)] would return the path without the names of the yaml file
     yml_file = source_dir.join(source_path[source_path.rfind(os.sep):]) # source_path[source_path.rfind(os.sep):] would return the name of the yaml file
     yml_file.write(schema_yml)
@@ -43,7 +53,7 @@ def test_check_source_folder_constraint(schema_yml, expected_status_code, source
         argv=[
             str(yml_file),
             "--source-folder",
-            str(tmpdir.join(desired_path)),
+            str(tmpdir.join(undesired_path)),
         ],
     )
     assert result == expected_status_code
