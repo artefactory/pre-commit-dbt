cb6ea3352eb6d2419db18f491c55e7eb70c6f42d
Miriam BENALLOU <miriam.benallou@MacBook-Air-de-admintech.local>
2022-07-08 11:18:37 +0200

    Add changes discussed

--- pre_commit_dbt/check_macro_is_referenced.py
+++ pre_commit_dbt/check_macro_is_referenced.py
@@ -1,7 +1,4 @@
 import argparse
-import json
-from pathlib import Path
-from posixpath import split
 from typing import Any
 from typing import Dict
 from typing import Optional
@@ -9,43 +6,49 @@ from typing import Sequence
 
 from pre_commit_dbt.utils import add_filenames_args
 from pre_commit_dbt.utils import add_manifest_args
-from pre_commit_dbt.utils import get_filenames
 from pre_commit_dbt.utils import get_json
-from pre_commit_dbt.utils import get_macro_schemas
 from pre_commit_dbt.utils import get_macro_sqls
 from pre_commit_dbt.utils import get_macros
 from pre_commit_dbt.utils import JsonOpenError
 
+def get_macro_name(macros_ref):
+    return map(lambda x:x.split(".")[-1], macros_ref) 
 
-def has_reference(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
+def get_macros_in_models(manifest):
+    """
+    Gets macros listed in the "depends_on" key of the "nodes" key in the manifest
+    Arguments:
+        manifest: manifest file 
+    Returns:
+        macros: list of macros
+    """
+    macros = []
+    nodes = manifest.get("nodes", {})
+    for key,value in nodes.items() :
+            macros_ref_in_models = value.get("depends_on",{}).get("macros",{})
+            macro_ref_name = get_macro_name(macros_ref_in_models) 
+            macros.extend(macro_ref_name)
+    return macros
+
+def is_referenced(paths: Sequence[str], manifest: Dict[str, Any]) -> int:
     status_code = 0
     sqls = get_macro_sqls(paths, manifest)
     filenames = set(sqls.keys())
-    nodes = manifest.get("nodes", {})
 
     # get manifest macros that pre-commit found as changed
-    macros = get_macros(manifest, filenames)
-    for macro in macros :
-        print(macro)
-        macro_name = macro.macro_name
-        found = False
-        for key,value in nodes.items() :
-            macros_as_declared_in_manifest = value.get("depends_on",{}).get("macros",{})
-            # maps original list elements which have the following format macro.[project_name].[macro_name] to macro_name
-            macros_transformed_names = map(lambda x:x.split(".")[-1], macros_as_declared_in_manifest) 
-            if macro_name in macros_transformed_names:
-                found = True
-                break
-            else:
-                continue
-        if not found:
-            print(f'The macro {macro_name} was not referenced in any model.')
-            status_code = 1
+    macros = list(get_macros(manifest, filenames))
+    referenced_macros = get_macros_in_models(manifest)
+
+    macros_not_referenced = [x for x in macros if x.macro_name not in referenced_macros]
+    for macro in macros_not_referenced:
+        status_code = 1
+        print(f'The macro {macro.macro_name} was not referenced in any model.')
     return status_code
     
 def main(argv: Optional[Sequence[str]] = None) -> int :
      parser = argparse.ArgumentParser()
      add_filenames_args(parser)
+     
      add_manifest_args(parser)
      args = parser.parse_args(argv)
 
@@ -55,7 +58,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int :
          print(f"Unable to load manifest file ({e})")
          return 1
 
-     return has_reference(paths=args.filenames, manifest=manifest)
+     return is_referenced(paths=args.filenames, manifest=manifest)
 
 if __name__ == "__main__":
      exit(main())
